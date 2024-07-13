e642a4b979ab9f2ef7f15b9ed51e497bb45653c8
root <willianzyt@outlook.com>
2024-07-13 15:15:37 -0300

    Teste

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -139,6 +139,7 @@ class CategoryController extends AbstractController
         ]);
     }
 
+    // Teste
     // Mostrar todos os produtos de uma categoria
     #[Route('/{id}/products', name: 'app_category_products', methods: ['GET'])]
     #[AT\Get( summary: 'Mostrar todos os produtos de uma categoria')]
